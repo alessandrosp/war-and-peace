b60109e2deec91dda398b150b5f39c2b72e8f920
Alessandro <9991341+annoys-parrot@users.noreply.github.com>
2019-12-31 11:14:42 +0000

    Update utils.js

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,10 +1,28 @@
 var utils = {
   
+  filterByName: function(name) {
+    $('character').each(function() {
+      // If name is empty, then we display all characters; if not
+      // then we check whether the name inputted is a substr of the name.
+      if (!name) {
+        $(this).css('display', 'block');
+      } else if (this.id.includes(name)) {
+        $(this).css('display', 'block');
+      } else {
+        $(this).css('display', 'none');
+      }
+    });
+  },
+  
   /**
    * Test whether the utils module has been loaded correctly.
    */
   testLog: function() {
-    console.log('This is just a test.')
+    console.log('This is just a test.');
   },
 
 };
+
+$('#filter-name').change(function() {
+  utils.filterByName(this.value);
+});
